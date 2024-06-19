b94bc65c7ed0edb6ad2c4b167c76617eef19a1ff
franciscobaez <9496@holbertonstudents.com>
2024-06-19 13:23:19 -0400

    c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -4,7 +4,8 @@
  * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
-int main(void);
+
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -14,6 +15,6 @@ int main(void);
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar("\n");
+	_putchar('\n');
 	return (0);
 }
