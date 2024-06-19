f3777f0864eaf4262def33238673b96888cc80f5
franciscobaez <9496@holbertonstudents.com>
2024-06-19 12:42:58 -0400

    c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,20 @@
 #include "main.h"
-
 /**
  * main - Entry point
  *
  * Return 0 (Success)
  */
-int _putchar(char c)
+int main(void)
 {
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	putchar('\n');
 	return (0);
-
 }
