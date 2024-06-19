c33b8755266cb11cedc9cf000f5a74e2f7e90aa4
franciscobaez <9496@holbertonstudents.com>
2024-06-18 21:17:52 -0400

    c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-        int num;
+	int num;
 
-        num = 0;
-        for (num = 0; num <= 9; num++)
-        {
-                putchar(num + '0');
-        }
+	num = 0;
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+	}
 
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
