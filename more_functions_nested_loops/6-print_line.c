4e87290d2b3a50e0f589b744ffeb4017e286f306
franciscobaez <9496@holbertonstudents.com>
2024-06-20 20:48:38 -0400

    c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,14 +9,10 @@ void print_line(int n)
 {
 	int j;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
-	}
-	else
-	   for (j = 0; j < n; j++)
-		{
+		for (j = 0; j < n; j++)
 			_putchar('_');
-		}
+	}
 	_putchar('\n');
 }
