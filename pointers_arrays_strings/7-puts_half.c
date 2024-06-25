818ac68e5d9330a19c1278000fd22b3ccd63a6c8
franciscobaez <9496@holbertonstudents.com>
2024-06-24 20:12:35 -0400

    c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * puts_half - Prints half the string.
- * Followed by a new line.
- * @str: String to print.
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
  */
 void puts_half(char *str)
 {
@@ -22,10 +22,9 @@ void puts_half(char *str)
 		{
 			_putchar(str[i]);
 		}
-	}
-	else if (len % 2)
+	} else if (len % 2)
 	{
-		for (n = (len - 1) / 2; n < len - 1; n--)
+		for (n = (len - 1) / 2; n < len - 1; n++)
 		{
 			_putchar(str[n + 1]);
 		}
