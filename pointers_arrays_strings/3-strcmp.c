5f47898bbed08697fd7a19b8e1e76b37ca5d44cd
franciscobaez <9496@holbertonstudents.com>
2024-06-26 12:01:37 -0400

    c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,6 @@
  * _strcmp - Compares two strings.
  * @s1: String to be compared with s2.
  * @s2: String to be compared with s1.
- * @result: Result of the comparing of strings.
  *
  * Return: 0 if equal, <0 if less and >0 if more.
  */
