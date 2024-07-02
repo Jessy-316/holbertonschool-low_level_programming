fa96fa1b5a14745987a2b4e272c9bcdc1d69435c
franciscobaez <9496@holbertonstudents.com>
2024-07-02 12:14:47 -0400

    c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Main header.
+ * @argc: Number of command lines.
+ * @argv: Size of array.
+ *
+ * Return: Name of file.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argv[i]);
+	}
+	printf("\n");
+	return (0);
+}
