473bc2f8339c0a1dde8812d825e7a2f5bcb6696c
franciscobaez <9496@holbertonstudents.com>
2024-07-11 13:03:49 -0400

    c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -16,14 +16,15 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i = 0;
+	int i;
 
+	i = 0;
 	while (ops[i].op != NULL)
-        {
-                if (strcmp(ops[i].op, s) == 0)
-                        return (ops[i].f);
-                i++;
-        }
-        return (0);
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (0);
 }
 
