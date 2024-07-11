9c213527f61da75e65d771e4fc521c7c2eda1112
franciscobaez <9496@holbertonstudents.com>
2024-07-11 10:45:18 -0400

    c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,1 +1,35 @@
-#include
+#include "calc.h"
+#include <stdio.h>
+/**
+ * main - Main function.
+ * @argc: Argument count.
+ * @argv: String of arguments in array.
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int count = 0;
+	get_op_func *result;
+
+	if (argc[count] != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+	if (argv[2] == "/") || (argv[2] == "%")) && (argv[3] == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+
+	result = get_op_func(argv[2])(num1, num2);
+	printf(result);
+}
