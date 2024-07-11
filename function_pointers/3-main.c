32fd576edfc8bb5cf54cbe2f41e2d93370be268f
franciscobaez <9496@holbertonstudents.com>
2024-07-11 10:53:50 -0400

    c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -13,23 +13,21 @@ int main(int argc, char *argv[])
 	get_op_func *result;
 
 	if (argc[count] != 4)
-	{
+
 		printf("Error\n");
 		return (98);
-	}
+
 	if (get_op_func(argv[2]) == NULL)
-	{
 		printf("Error\n");
 		return (99);
-	}
-	if (argv[2] == "/") || (argv[2] == "%")) && (argv[3] == 0)
-	{
+
+	if (((argv[2] == "/") || (argv[2] == "%")) && (argv[3] == 0))
 		printf("Error\n");
 		return (100);
-	}
+
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[2]);
 
 	result = get_op_func(argv[2])(num1, num2);
-	printf(result);
+		printf(result);
 }
