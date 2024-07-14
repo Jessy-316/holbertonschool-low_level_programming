b412152fa90d161738b80514a94b1e9e5282bf3b
franciscobaez <9496@holbertonstudents.com>
2024-07-13 23:22:33 -0400

    c

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -6,4 +6,14 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void printchar(va_list print);
+void printint(va_list print);
+void printfloat(va_list print);
+void printstr(va_list print);
+
+typedef struct check
+{
+  char *type;
+  void (*f)();
+} check;
 #endif /* VARIADIC_FUNCTIONS_H */
