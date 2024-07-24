2c1c2bda3181ed173b136715d08019d7b6186c8a
franciscobaez <9496@holbertonstudents.com>
2024-07-24 12:56:03 -0400

    betty corrected the lists.h file, and finished the 0-print_dlistint.c file

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_.H
-#define LISTS.H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdio.h>
 #include <stddef.h>
@@ -16,9 +16,9 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+int n;
+struct dlistint_s *prev;
+struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
