439bc677b4b15a9e99bb25ccf50a69056a1f6388
franciscobaez <9496@holbertonstudents.com>
2024-07-28 22:14:23 -0400

    Function that creates a file.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fildes = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR, S_IWUSR);
+	fildes = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fildes == -1)
 	{
 		return (-1);
