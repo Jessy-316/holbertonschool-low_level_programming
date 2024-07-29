d9a9829c6cf691c4e820ad98c77fef8349adbbd3
franciscobaez <9496@holbertonstudents.com>
2024-07-28 23:11:55 -0400

    changed the code to comply with intranet requirements

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,22 +8,22 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fildes, length, wr_count;
+	int new_file, len, wr_stat;
 
 	if (filename == NULL)
 		return (-1);
-	fildes = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fildes == -1)
+	new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (new_file == -1)
 		return (-1);
 	if (text_content == NULL)
 	{
-		close(fildes);
+		close(new_file);
 		return (1);
 	}
-	for (length = 0; text_content[length]; length++)
+	for (len = 0; text_content[len]; len++)
 		;
-	wr_count = write(fildes, text_content, length);
-	if (close(fildes) == -1)
+	wr_stat = write(new_file, text_content, len);
+	if (close(new_file) == -1)
 		return (-1);
-	return (wr_count == -1 ? -1 : 1);
+	return (wr_stat == -1 ? -1 : 1);
 }
