44e6d42f293d9e14ca953482a20904a8e2d4489d
franciscobaez <9496@holbertonstudents.com>
2024-07-28 21:51:54 -0400

    Function that creates a file.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,10 +16,6 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	else if (chmod(filename, S_IWUSR) == -1)
-	{
-		return (-1);
-	}
 	fildes = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fildes == -1)
 	{
